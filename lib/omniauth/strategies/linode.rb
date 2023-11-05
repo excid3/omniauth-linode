30ad8f1161758fa3c6b8daa3ad09a6317f46bf4a
Chris Oliver <excid3@gmail.com>
2023-11-04 20:11:25 -0500

    Use auth_scheme instead of overriding methods

--- lib/omniauth/strategies/linode.rb
+++ lib/omniauth/strategies/linode.rb
@@ -9,39 +9,29 @@ module OmniAuth
       option :name, 'linode'
 
       option :client_options, {
-        :site => 'https://login.linode.com',
-        :authorize_path => '/oauth/authorize',
-        :token_url => '/oauth/token',
+        auth_scheme: :request_body,
+        site: 'https://login.linode.com',
+        authorize_path: '/oauth/authorize',
+        token_url: '/oauth/token',
       }
 
       uid { raw_info['uid'] }
 
       info do
         {
-          name:       raw_info['username'],
-          email:      raw_info['email'],
-          nickname:   raw_info['username']
+          name: raw_info['username'],
+          email: raw_info['email'],
+          nickname: raw_info['username']
         }
       end
 
       extra do
-        {
-          'raw_info' => raw_info
-        }
+        { 'raw_info' => raw_info }
       end
 
       def raw_info
         @raw_info ||= access_token.get('https://api.linode.com/v4/profile').parsed
       end
-
-      protected
-
-      def build_access_token
-        verifier = request.params["code"]
-        params = {:redirect_uri => callback_url, :state => request.params["state"], :client_id => client.id, :client_secret => client.secret}
-        client.auth_code.get_token(verifier, params.merge(token_params.to_hash(:symbolize_keys => true)), deep_symbolize(options.auth_token_params))
-      end
-
     end
   end
 end
